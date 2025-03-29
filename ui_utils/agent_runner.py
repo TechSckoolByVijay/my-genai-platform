3d7238a7644e0aeb4014b745d2343890269a3a95
YourUsername <youremail@example.com>
2025-03-29 19:26:33 +0530

    cfd

--- ui_utils/agent_runner.py
+++ ui_utils/agent_runner.py
@@ -4,7 +4,12 @@ import os
 from dotenv import load_dotenv
 
 sys.path.append(os.path.join(os.getcwd(), 'crews/interview_researcher/src'))
-from interview_researcher import main
+print("Python Path:", sys.path)
+
+#from interview_researcher import main
+
+from crews.interview_researcher.src.interview_researcher import main
+
 
 #from crews.interview_researcher.src.interview_researcher.main import main
 load_dotenv(os.path.join(os.getcwd(), 'crews/interview_researcher', '.env'))
